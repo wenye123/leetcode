7eb252b2a13669726d8f17c46a97e9f32793e42f
wenye <1583247975@qq.com>
2022-06-02 11:02:02 +0800

    feat: 排序-改进算法

--- src/base/Sort.ts
+++ src/base/Sort.ts
@@ -6,6 +6,13 @@
  *
  */
 
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+const SHOW_LOG: boolean = false;
+function log(msg: string, ...args: any[]) {
+  if (SHOW_LOG !== true) return;
+  console.log(msg, ...args);
+}
+
 function swap(nums: number[], a: number, b: number) {
   const tmp = nums[a];
   nums[a] = nums[b];
@@ -89,20 +96,130 @@ export function insertSort(nums: number[]) {
   let j = 0,
     current = 0;
   for (let i = 1; i < nums.length; i++) {
+    // 默认从第二个元素向前
     current = nums[i];
     for (j = i - 1; j >= 0 && nums[j] > current; j--) {
+      // 向前过程如果值比自己大则赋值当前
       nums[j + 1] = nums[j];
     }
-    nums[j + 1] = current;
+    nums[j + 1] = current; // 如果没有比自己大的则写入当前值
   }
 }
 
 /************************ 改进算法 ************************** */
 
-/** 快速排序 */
-export function fast(nums: number[]): number[] {
+/**
+ * 希尔排序(插入排序的改进)
+ * 通过增量分组方式分割成若干个子序列，对子序列进行插入排序，当整个序列基本有序时候，在对全体进行一次插入排序
+ */
+export function shellSort(nums: number[]) {
+  let j = 0,
+    current = 0;
+  // 增量减少 直到为1(1就是正常的直接插入排序)
+  for (let increment = Math.floor(nums.length / 2); increment > 0; increment = Math.floor(increment / 2)) {
+    for (let i = increment; i < nums.length; i++) {
+      current = nums[i];
+      for (j = i - increment; j >= 0 && nums[j] > current; j -= increment) {
+        // 默认从递增量向前
+        nums[j + increment] = nums[j];
+      }
+      nums[j + increment] = current;
+    }
+  }
+}
+
+/**
+ * 归并排序(2路归并)
+ *   采用分治的思想 原数组拆分为多个子序列 先让子序列有序 最后达到整体有序
+ *   复杂度: 每次合并操作的平均复杂度是o(n) 树的深度是log2n 因此总的平均复杂度为O(nlogn)，空间复杂度O(n)
+ *   比较占内存 效率高且稳定
+ */
+function merge(leftNums: number[], rightNums: number[]) {
+  const tmp: number[] = [];
+  let i = 0,
+    j = 0;
+  // 两个有序子序列进行对比 值小的推进临时数组中 直到其中一个数组遍历完
+  while (i < leftNums.length && j < rightNums.length) {
+    if (leftNums[i] < rightNums[j]) {
+      tmp.push(leftNums[i++]);
+    } else {
+      tmp.push(rightNums[j++]);
+    }
+  }
+  // 将左边剩余的添加到临时数组
+  while (i < leftNums.length) {
+    tmp.push(leftNums[i++]);
+  }
+  // 将右边剩余的添加到临时数组
+  while (j < rightNums.length) {
+    tmp.push(rightNums[j++]);
+  }
+  // 返回临时数组
+  return tmp;
+}
+
+// 归并排序-递归版
+export function mergeSort1(nums: number[]): number[] {
+  if (nums.length === 1) return nums;
+  const midIndex = Math.floor(nums.length / 2);
+  const leftNums = nums.slice(0, midIndex);
+  const rightNums = nums.slice(midIndex);
+  return merge(mergeSort1(leftNums), mergeSort1(rightNums));
+}
+
+// 归并排序-循环递归版
+export function mergeSort2(nums: number[]) {
+  // 步长为1 每轮翻倍 直到最大为数组长度
+  for (let step = 1; step <= nums.length; step = 2 * step) {
+    log("----step----", step);
+    for (let index = 0; index + step <= nums.length; index += 2 * step) {
+      // 计算子序列索引
+      const left = index;
+      const middle = index + step;
+      let right = index + 2 * step;
+      if (right >= nums.length) {
+        // 如果右边的值超过数组长度则重置为数组长度
+        right = nums.length;
+      }
+      // 获取子序列
+      const leftNums = nums.slice(left, middle);
+      const rightNums = nums.slice(middle, right);
+      // 排序子序列
+      const mergeArr = merge(leftNums, rightNums);
+      log("merge", left, middle, right, leftNums, rightNums, mergeArr);
+      // 将排序后的子序列写回原数组
+      for (let i = left; i < right; i++) {
+        nums[i] = mergeArr[i - left];
+      }
+    }
+  }
+}
+
+/**
+ * 快速排序
+ *   步骤
+ *     数组中选择一个元素作为基点
+ *     排序数组 所有比基点大的值放左边 否则放右边 最后基点插入在中间
+ *     递归左右数组
+ *   时间复杂度为最优O(logn)最差o(n^2) 空间复杂度最优O(logn)最差o(n) 不稳定
+ */
+
+// 通过双指针进行分区
+function partition(nums: number[], left: number, right: number) {
+  const pivotVal = nums[left]; // 选择左边索引值作为基准
+  while (left < right) {
+    while (left < right && nums[right] >= pivotVal) right--;
+    swap(nums, left, right);
+    while (left < right && nums[left] <= pivotVal) left++;
+    swap(nums, left, right);
+  }
+  return left;
+}
+
+// 快速排序-递归内存版
+export function fastSort1(nums: number[]): number[] {
   if (nums.length <= 1) return nums;
-  const index = Math.floor(nums.length / 2);
+  const index = Math.floor(nums.length / 2); // 取中间值当基点
   const curr = nums[index];
   const left = [];
   const right = [];
@@ -115,8 +232,42 @@ export function fast(nums: number[]): number[] {
       right.push(nums[i]);
     }
   }
-  return [...fast(left), curr, ...fast(right)];
+  return [...fastSort1(left), curr, ...fastSort1(right)];
 }
 
-const nums = [6, 3, 1, 8];
-console.log(fast(nums));
+// 快速排序-递归分区版
+function _fastSort2(nums: number[], left: number, right: number) {
+  if (left < right) {
+    const pivot = partition(nums, left, right);
+    _fastSort2(nums, left, pivot - 1);
+    _fastSort2(nums, pivot + 1, right);
+  }
+}
+export function fastSort2(nums: number[]) {
+  _fastSort2(nums, 0, nums.length - 1);
+}
+
+// 快速排序-栈循环分区版
+export function fastSort3(nums: number[]) {
+  // 定义一个栈
+  const stack: Array<number[]> = [];
+  // 将初始左右索引推入栈中
+  stack.push([0, nums.length - 1]);
+  log("stack", stack);
+  // 只要存在未排序的子数组则循环
+  while (stack.length > 0) {
+    const [left, right] = stack.pop() as number[];
+    log("pop", [left, right]);
+    const pivot = partition(nums, left, right);
+    // 左侧有未排序元素则推入栈
+    if (pivot - 1 > left) {
+      stack.push([left, pivot - 1]);
+      log("stack", stack);
+    }
+    // 右侧有未排序元素则推入栈
+    if (pivot + 1 < right) {
+      stack.push([pivot + 1, right]);
+      log("stack", stack);
+    }
+  }
+}
--- test/base/test-index.ts
+++ test/base/test-index.ts
@@ -1,6 +1,18 @@
 import { binarySearch, sentrySearch, sentrySearch2 } from "../../src/base/Search";
 import { assert } from "chai";
-import { bubbleSort, bubbleSort2, insertSort, swapSort, swapSort2 } from "../../src/base/Sort";
+import {
+  bubbleSort,
+  bubbleSort2,
+  fastSort1,
+  fastSort2,
+  fastSort3,
+  insertSort,
+  mergeSort1,
+  mergeSort2,
+  shellSort,
+  swapSort,
+  swapSort2,
+} from "../../src/base/Sort";
 
 describe("基础", function () {
   describe("查找", function () {
@@ -25,29 +37,55 @@ describe("基础", function () {
   });
   describe.only("排序", function () {
     let nums: number[] = [];
+    let sortNums = [1, 2, 3, 4, 6, 8, 9];
     beforeEach(() => {
-      nums = [6, 3, 1, 8, 4, 2];
+      nums = [6, 3, 1, 8, 4, 2, 9];
     });
 
-    it("交换排序", function () {
+    it("简单算法-交换排序", function () {
       swapSort(nums);
-      assert.deepStrictEqual(nums, [1, 2, 3, 4, 6, 8]);
+      assert.deepStrictEqual(nums, sortNums);
     });
-    it("简单选择排序(交换排序改进版)", function () {
+    it("简单算法-简单选择排序(交换排序改进版)", function () {
       swapSort2(nums);
-      assert.deepStrictEqual(nums, [1, 2, 3, 4, 6, 8]);
+      assert.deepStrictEqual(nums, sortNums);
     });
-    it("冒泡排序", function () {
+    it("简单算法-冒泡排序", function () {
       bubbleSort(nums);
-      assert.deepStrictEqual(nums, [1, 2, 3, 4, 6, 8]);
+      assert.deepStrictEqual(nums, sortNums);
     });
-    it("冒泡排序改进版", function () {
+    it("简单算法-冒泡排序改进版", function () {
       bubbleSort2(nums);
-      assert.deepStrictEqual(nums, [1, 2, 3, 4, 6, 8]);
+      assert.deepStrictEqual(nums, sortNums);
     });
-    it("插入排序", function () {
+    it("简单算法-插入排序", function () {
       insertSort(nums);
-      assert.deepStrictEqual(nums, [1, 2, 3, 4, 6, 8]);
+      assert.deepStrictEqual(nums, sortNums);
+    });
+
+    it("改进算法-希尔排序", function () {
+      shellSort(nums);
+      assert.deepStrictEqual(nums, sortNums);
+    });
+    it("改进算法-归并排序-递归版", function () {
+      const arr = mergeSort1(nums);
+      assert.deepStrictEqual(arr, sortNums);
+    });
+    it("改进算法-归并排序-循环递归版", function () {
+      mergeSort2(nums);
+      assert.deepStrictEqual(nums, sortNums);
+    });
+    it("改进算法-快速排序-递归内存版", function () {
+      const arr = fastSort1(nums);
+      assert.deepStrictEqual(arr, sortNums);
+    });
+    it("改进算法-快速排序-递归分区版", function () {
+      fastSort2(nums);
+      assert.deepStrictEqual(nums, sortNums);
+    });
+    it("改进算法-快速排序-栈循环分区版", function () {
+      fastSort3(nums);
+      assert.deepStrictEqual(nums, sortNums);
     });
   });
 });
